ca24e8b3fd77a7beaeb44954708c4a6ee654536a
Rob J. Schaefer <schae234@umn.edu>
2013-08-28 06:43:22 -0500

    finished implementing save/load feature of camoco; added test script

--- camoco/sample.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-# Copyright 2013 Rob Schaefer <schae234@gmail.com>
-
-
-class Sample(object):
-    ''' An object which descripted an experimental Sample'''
-    def __init__(self):
---
+++ tests/camoco_tests.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+from camoco import Camoco
+from reference import Reference
+
+c = Camoco()
+ZmB73_5a = Reference("Maize B73 Genome Build 5a","~/Codes/root_pipeline/pypeline/tests/data/rCRS.fasta")
