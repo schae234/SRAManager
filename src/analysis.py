7efee1ff13e232e180d064501d80979bc7858734
Rob J. Schaefer <schae234@umn.edu>
2013-09-06 11:55:55 -0500

    added analysis skeleton class

---
+++ src/analysis.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import camoco as camoco
+import pypeline as pypeline
+
+class Analysis(object):
+    ''' This class implements a analyis done on a camoco object'''
+
+    def __init__(self, camoco_inst):
+        ''' constructor for analysis '''
+        self.
+   
+    def run(self):
+        ''' run an analysis '''
+        pass
+
+
+    def store(self):
+        ''' store analysis results '''
+ 
